c8b941d627068efbae11ab6f1152261f83066eab
Dima Dooev <dooevdima3@gmail.com>
2018-09-09 05:24:14 +0300

    Подгрузка блоков

--- www/js/common.js
+++ www/js/common.js
@@ -12,16 +12,16 @@ window.addEventListener('DOMContentLoaded', function() {
          // popup_gift.classList.remove('hide');
          btn_gift.classList.remove('infinite');
          btn_gift.classList.remove('pulse');
-         setTimeout(function() { 
-            btn_gift.classList.add('hide'); 
+         setTimeout(function() {
+            btn_gift.classList.add('hide');
          }, 500);
       });
 
       popup_gift.addEventListener('click', function(event) {
          let target = event.target;
-         if (target.className != 'popup-content' && 
+         if (target.className != 'popup-content' &&
             target.parentNode.className != 'popup-content' ||
-             target.className === 'popup-close') {
+            target.className === 'popup-close') {
             popup_gift.classList.remove('show');
             // popup_gift.classList.add('hide');
          }
@@ -32,68 +32,95 @@ window.addEventListener('DOMContentLoaded', function() {
 
    // showDesign START Модальные окна popup-design 
    function showDesign() {
-     
+
       let btn_design = document.querySelectorAll('.button-design'),
-          popup_design = document.querySelector('.popup-design');
+         popup_design = document.querySelector('.popup-design'),
+         popup_content = document.querySelectorAll('.popup-content');
 
       document.addEventListener('click', function(event) {
          let target = event.target;
-         if ( /button-design/g.test(target.className) ) {
+         if (/button-design/g.test(target.className)) {
             popup_design.classList.add('show');
          }
       });
 
-      popup_design.addEventListener('click', function(event) {
-         let target = event.target;
-         if (target.className != 'popup-dialog' && 
-            target.parentNode.className != 'popup-dialog' ||
-             target.className === 'popup-close') {
-            popup_design.classList.remove('show');
-            popup_design.classList.add('hide');
-         }
-      });
+      for (var i = 0; i < popup_content.length; i++) {
+         popup_content[i].addEventListener('click', function() {
+            // let target = event.target;
+            if (this.className === 'popup-content') {
+               popup_design.classList.remove('hide');
+               popup_design.classList.add('show');
+            }
+         });
+      };
    }
+
    showDesign();
    // showDesign END Модальные окна popup-design 
 
-   // For All START
-   // function hasClassName(elem, cl) {
-   //   return (new RegExp('^|\s' + cl + '\s|$')).test(elem.className);
-   // }
+// showMoreStyle START Подгрузка блоков
+   function showMoreStyle () {
+      let btnMoreStyle = document.querySelector('.button-transparent'),
+          blockMoreStyle = document.querySelectorAll('.styles-2');
 
+      btnMoreStyle.addEventListener('click', function(){
+         for (var i = 0; i < blockMoreStyle.length; i++) {
+            blockMoreStyle[i].classList.remove('hidden-lg', 'hidden-md', 'hidden-sm', 'hidden-xs', 'styles-2');
+            blockMoreStyle[i].classList.add('col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1');
+            btnMoreStyle.classList.add('hide');
 
-   // function showPopup(className, popupName) {
-   
-   //       let target = event.target;
-   //       if (hasClassName(target, className)) {
-   //          popupName.classList.add('show');
-   //       }
-   //    });
-
-
-   // }
-
-   // btn_design.addEventListener('click', showPopup(event 'des', popup_design));
+         }
+      });
+   }
+   showMoreStyle();
+// showMoreStyle END Подгрузка блоков
 
 
 
-   // function hidePopup(namePopup, classPopup){
-   //    document.addEventListener('click', function(event) {
-   //       let target = event.target;
-   //       if (target.className != 'popup-content' && 
-   //           target.parentNode.className != 'popup-content' ||
-   //           target.className === 'popup-close') {
-   //           namePopup.classList.remove('show');
-   //       // popup_gift.classList.add('hide');
-   //       }
-   //    });
-   // }
-      // hidePopup(popup_gift, 'popup-content');
+}); // GENERAL END 
 
-   // For All END  
+// For All START
 
-   //  START
-   //  END
-});
+
+
+
+
+// function hasClassName(elem, cl) {
+//   return (new RegExp('^|\s' + cl + '\s|$')).test(elem.className);
+// }
+
+
+// function showPopup(className, popupName) {
+
+//       let target = event.target;
+//       if (hasClassName(target, className)) {
+//          popupName.classList.add('show');
+//       }
+//    });
+
+
+// }
+
+// btn_design.addEventListener('click', showPopup(event 'des', popup_design));
+
+
+
+// function hidePopup(namePopup, classPopup){
+//    document.addEventListener('click', function(event) {
+//       let target = event.target;
+//       if (target.className != 'popup-content' && 
+//           target.parentNode.className != 'popup-content' ||
+//           target.className === 'popup-close') {
+//           namePopup.classList.remove('show');
+//       // popup_gift.classList.add('hide');
+//       }
+//    });
+// }
+// hidePopup(popup_gift, 'popup-content');
+
+// For All END  
+
+//  START
+//  END
 //  START
 //  END
